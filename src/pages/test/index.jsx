aa09859f1f1dd81b670d3f3b503df5d0a3c043de
jifeng <jifeng.pjf@aistarfish.com>
2022-08-05 18:05:59 +0800

    feat: 快捷输入功能

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,22 +1,118 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { message } from 'antd';
 import './index.scss';
+
+const checkList = [
+	{
+		id: '1',
+		name: '血常规'
+	},
+	{
+		id: '2',
+		name: '血生化'
+	},
+	{
+		id: '3',
+		name: '肝功能'
+	},
+	{
+		id: '4',
+		name: '肾功能'
+	},
+	{
+		id: '5',
+		name: '电解质'
+	}
+]
+
+let id = 0
 
 export default () => {
 
-	const onSelect = () => {
-		console.log(document.getSelection())
-		const res = document.execCommand('insertHTML', false, '<span>血生化</span>');
-		console.log(res)
+	const [html, setHtml] = useState(''); // html数据
+	const [tempHtml, setTempHtml]= useState('')
+	const [previousInfo, setPreviousInfo] = useState({}); // 光标位置
+	const [parentNode, setParentNode] = useState();
+	const editorRef = useRef();
+
+	useEffect(() => {
+		renderHtml(tempHtml)
+	}, [tempHtml]);
+
+	const renderHtml = (html) => {
+		if (!html) {
+			return;
+		}
+		console.log('html：', html)
+		// editorRef.current.innerHTML = `<span id=${++id}>${html}</span>`;
 	}
+
+	const onSelect = (check) => {
+		editorRef.current.focus();
+
+		if (parentNode?.className === 'tag') {
+			message.error('此处无法插入引用');
+			return;
+		}
+
+		const { offset, previousSibling } = previousInfo
+
+		let position = offset || 0;
+		let find = false;
+		if (previousSibling) {
+			editorRef.current.childNodes.forEach(node => {
+				if (!find) {
+					if (node.id !== previousSibling.id) {
+						position += node.nodeName === 'SPAN' ? 44 + node.textContent.length : node.textContent.length
+						console.log('position1', position)
+					} else {
+						position += node.nodeName === 'SPAN' ? 44 + node.textContent.length : node.textContent.length
+						find = true
+						console.log('position2', position)
+					}
+				}
+			})
+		}
+		const nodeId = new Date().getTime() + '';
+		const newHtml = html.slice(0, position) + `<span id="${nodeId}" class="tag">#${check.name}</span>` + html.slice(position);
+
+		editorRef.current.innerHTML = newHtml;
+		setHtml(newHtml)
+		setTimeout(() => {
+			const newNode = document.getElementById(nodeId);
+			const sel = window.getSelection();
+			sel.removeAllRanges();
+			var range = document.createRange();
+			// 设置选中节点的当前range
+  		range.selectNode(newNode);
+  		sel.addRange(range);
+  		// 光标移到节点之后
+			sel.collapseToEnd();
+		}, 100);
+	}
+
+	const onBlur = (e) => {
+		const { anchorNode, anchorOffset } = getSelection();
+		setPreviousInfo({
+			previousSibling: anchorNode?.previousSibling,
+			offset: anchorOffset,
+		});
+	}
+
+	const onInput = (e) => {
+		// console.log(editorRef.current.textContent); // 获取纯文本
+		setHtml(editorRef.current.innerHTML);
+	}
+
 
 	return (
 		<div className="test-container">
+
 			<div className="tag-box">
-				<div onClick={onSelect}>血常规</div>
-				<div onClick={onSelect}>血生化</div>
-				<div onClick={onSelect}>CTCTCTCT</div>
+				{checkList.map(check => <div key={check.id} onClick={() => onSelect(check)}>{check.name}</div>)}
 			</div>
-			<div className="editor" contentEditable="true">
+
+			<div ref={editorRef} className="editor" contentEditable="true" onInput={onInput} onBlur={onBlur}>
 				
 			</div>
 		</div>
