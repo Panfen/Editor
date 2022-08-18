2c77b3ff6c280aed8471ad49559f7e50a5900e8e
jifeng <jifeng.pjf@aistarfish.com>
2022-08-18 19:59:26 +0800

    feat: html编辑模式

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -4,32 +4,32 @@ import './index.scss';
 
 const checkList = [
 	{
-		id: '1',
+		key: '1',
 		name: '血常规'
 	},
 	{
-		id: '2',
+		key: '2',
 		name: '血生化'
 	},
 	{
-		id: '3',
+		key: '3',
 		name: '肝功能'
 	},
 	{
-		id: '4',
+		key: '4',
 		name: '肾功能'
 	},
 	{
-		id: '5',
+		key: '5',
 		name: '电解质'
 	}
 ]
 
 let id = 0
 
-export default () => {
+export default (props) => {
 
-	const [html, setHtml] = useState(''); // html数据
+	const [html, setHtml] = useState('')
 	const [tempHtml, setTempHtml]= useState('')
 	const [previousInfo, setPreviousInfo] = useState({}); // 光标位置
 	const [parentNode, setParentNode] = useState();
@@ -37,54 +37,35 @@ export default () => {
 	const editorRef = useRef();
 
 	useEffect(() => {
-		renderHtml(tempHtml)
-	}, [tempHtml]);
+		initData();
+	}, []);
 
-	const renderHtml = (html) => {
-		if (!html) {
-			return;
-		}
-		console.log('html：', html)
-		// editorRef.current.innerHTML = `<span id=${++id}>${html}</span>`;
+	const initData = () => {
+		setHtml(`<div><span id="INITTIAL_ID"></span></div>`)
 	}
 
 	const onSelect = (check) => {
 		editorRef.current.focus();
 
-		if (parentNode?.className === 'tag') {
-			message.error('此处无法插入引用');
-			return;
+		const { offset = 0, id = 'INITTIAL_ID' } = previousInfo;
+		const targetNode = document.getElementById(id);
+		const content = targetNode.textContent;
+		// 前部分文字处理
+		targetNode.textContent = content.substr(0, offset);
+		// 插入引用项
+		const tagNode = createNode(check.key, 'tag', '#' + check.name);
+		targetNode.parentNode.insertBefore(tagNode, targetNode.nextSibling);
+
+		if (offset < content.length) {
+			// 后部分文字处理
+			const textNode = createNode('', '', content.substr(offset));
+			targetNode.parentNode.insertBefore(textNode, tagNode.nextSibling);
 		}
-
-		const { offset, previousSibling } = previousInfo
-
-		let position = offset || 0;
-		let find = false;
-		if (previousSibling) {
-			editorRef.current.childNodes.forEach(node => {
-				if (!find) {
-					if (node.id !== previousSibling.id) {
-						position += node.nodeName === 'SPAN' ? 44 + node.textContent.length : node.textContent.length
-						console.log('position1', position)
-					} else {
-						position += node.nodeName === 'SPAN' ? 44 + node.textContent.length : node.textContent.length
-						find = true
-						console.log('position2', position)
-					}
-				}
-			})
-		}
-		const nodeId = check.id + '-' + new Date().getTime();
-		const newHtml = html.slice(0, position) + `<span id="${nodeId}" class="tag">#${check.name}</span>` + html.slice(position);
-
-		editorRef.current.innerHTML = newHtml;
-		setHtml(newHtml)
-		const newNode = document.getElementById(nodeId);
-		const sel = window.getSelection();
 		
+		const sel = window.getSelection();
 		var range = document.createRange();
 		// 设置选中节点的当前range
-		range.selectNode(newNode);
+		range.selectNode(tagNode);
 		sel.removeAllRanges();
 		sel.addRange(range);
 		// 光标移到节点之后
@@ -92,10 +73,10 @@ export default () => {
 	}
 
 	const onBlur = (e) => {
-		console.log('onBlur')
-		const { anchorNode, anchorOffset } = getSelection();
+		const sel = window.getSelection();
+		const { anchorNode, anchorOffset } = sel;
 		setPreviousInfo({
-			previousSibling: anchorNode?.previousSibling,
+			id: anchorNode?.parentElement?.id,
 			offset: anchorOffset,
 		});
 	}
@@ -107,40 +88,92 @@ export default () => {
 		}
 	}
 
+	/**
+	 * 创建span节点
+	 */ 
+	const createNode = (id = '', className = '', text = '') => {
+		const newId = generateId(id);
+		const node = document.createElement('span');
+		node.setAttribute('id', newId);
+		node.setAttribute('class', className);
+		node.textContent = text;
+		return node;
+	}
+
+	const generateId = (id = '') => {
+		return id + '-' + new Date().getTime();
+	}
+
+	/**
+	 * 光标移动至某个节点后
+	 */ 
+	const collapseNode = (id) => {
+		setTimeout(() => {
+			const sel = window.getSelection();
+			var range = document.createRange();
+			// 设置选中节点的当前range
+			range.selectNode(document.getElementById(id));
+			sel.removeAllRanges();
+			sel.addRange(range);
+			// 光标移到节点之后
+			sel.collapseToEnd();
+		});
+	}
+
 	const onInput = (e) => {
 		// console.log(isTypeChinese)
-		// console.log(editorRef.current.textContent); // 获取纯文本
-		setHtml(editorRef.current.innerHTML);
+		if (editorRef.current.childNodes[0].nodeType === 3) {
+			const id = generateId()
+			setHtml(`<div><span id="${id}">${editorRef.current.textContent}</span></div>`);
+			collapseNode(id);
+		}
 
-		editorRef.current.childNodes.forEach((node, index) => {
-			const check = checkList.find(item => item.id === node.id?.split('-')[0]);
-			if (node.className === 'tag') {
-				if (node.textContent.length > check.name.length + 1) {
-					console.log(1)
-					// 新文本节点的内容
-					const textNodeCont = node.textContent.substr(check.name.length + 1);
-					// tag内容重置
-					node.textContent = `#${check.name}`;
-					// 添加文本节点
-					const textNode = document.createTextNode(textNodeCont)
-					editorRef.current.insertBefore(textNode, node.nextSibling);
-
-					const sel = window.getSelection();
-					var range = document.createRange();
-					// 设置选中节点的当前range
-					range.selectNode(textNode);
-					sel.removeAllRanges();
-					sel.addRange(range);
-					// 光标移到节点之后
-					sel.collapseToEnd();
-				} else if (node.textContent.length < check.name.length + 1) {
-					console.log(2)
-					editorRef.current.removeChild(node);
-				} else {
-					console.log(3)
+		
+		editorRef.current.childNodes.forEach((_node) => {
+			if (_node.nodeName === 'DIV') {
+				// 换行后为span添加id
+				_node.childNodes.forEach(childNode => {
+					if (!childNode?.getAttribute('id')) {
+						childNode.setAttribute('id', generateId());
+					}
+				});
+				// 换行后在上一行末尾添加分号
+				if (_node.previousSibling) {
+					const content = _node.previousSibling.lastChild.textContent;
+					if (!content.endsWith('；')) {
+						const textNode = createNode('', '', '；');
+						_node.previousSibling.appendChild(textNode);
+					}
 				}
 			}
-		})
+
+			// 处理引用项后输入和删除引用项
+			_node.childNodes.forEach((node) => {
+				const check = checkList.find(item => item.key === node.id?.split('-')[0]);
+				if (node.className === 'tag') {
+					if (node.textContent.length > check.name.length + 1) {
+						// 新文本节点的内容
+						const textNodeCont = node.textContent.substr(check.name.length + 1);
+						// 引用项内容重置
+						node.textContent = `#${check.name}`;
+						// 添加文本节点
+						const textNode = createNode('', '', textNodeCont)
+						_node.insertBefore(textNode, node.nextSibling);
+
+						const sel = window.getSelection();
+						var range = document.createRange();
+						// 设置选中节点的当前range
+						range.selectNode(textNode);
+						sel.removeAllRanges();
+						sel.addRange(range);
+						// 光标移到节点之后
+						sel.collapseToEnd();
+					} else if (node.textContent.length < check.name.length + 1) {
+						_node.removeChild(node);
+					}
+				}
+			})
+		});
 	}
 
 	/**
@@ -161,7 +194,7 @@ export default () => {
 		<div className="test-container">
 
 			<div className="tag-box">
-				{checkList.map(check => <div key={check.id} onClick={() => onSelect(check)}>{check.name}</div>)}
+				{checkList.map(check => <div key={check.key} onClick={() => onSelect(check)}>{check.name}</div>)}
 			</div>
 
 			<div 
@@ -171,6 +204,7 @@ export default () => {
 				onInput={onInput}
 				onClick={onClick}
 				onBlur={onBlur}
+				dangerouslySetInnerHTML={{ __html: html }}
 				onCompositionStart={onCompositionStart}
 				onCompositionEnd={onCompositionEnd}
 			/>
